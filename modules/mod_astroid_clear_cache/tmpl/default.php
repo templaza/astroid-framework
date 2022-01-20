4f33e9c700aa75a04373c0fba5b835e7835b6a9b
Sonny <sonvnn@gmail.com>
2022-01-20 17:03:19 +0700

    Add module Astroid Clear Cache

---
+++ modules/mod_astroid_clear_cache/tmpl/default.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Jollyany
+ * @author TemPlaza http://www.templaza.com
+ * @copyright Copyright (c) 2010 - 2022 Jollyany
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+
+$hideLinks = $app->input->getBool('hidemainmenu');
+if ($hideLinks) {
+	return;
+}
+?>
+<a class="header-item-content astroid-clear-cache"
+   href="#"
+   title="<?php echo Text::_('Astroid Clear Cache'); ?>">
+    <div class="header-item-icon">
+        <div class="w-auto">
+            <span class="icon-refresh" aria-hidden="true"></span>
+        </div>
+    </div>
+    <div class="header-item-text">
+		<?php echo Text::_('Astroid Clear Cache'); ?>
+    </div>
+</a>
--- script.php
+++ script.php
@@ -2,8 +2,8 @@
 
 /**
  * @package   Astroid Framework
- * @author    JoomDev https://www.joomdev.com
- * @copyright Copyright (C) 2009 - 2020 JoomDev.
+ * @author    TemPlaza https://www.templaza.com
+ * @copyright Copyright (C) 201 - 2022 TemPlaza.
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 
@@ -16,69 +16,124 @@ jimport('joomla.filesystem.file');
 class astroidInstallerScript
 {
 
-   /**
-    * 
-    * Function to run before installing the component	 
-    */
-   public function preflight($type, $parent)
-   {
-      $plugin_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'plugins' . '/';
-      $plugins = array_filter(glob($plugin_dir . '*'), 'is_dir');
-      foreach ($plugins as $plugin) {
-         if ($type == "uninstall") {
-            $this->uninstallPlugin($plugin, $plugin_dir);
-         }
-      }
-   }
+	/**
+	 *
+	 * Function to run before installing the component
+	 */
+	public function preflight($type, $parent)
+	{
+		$plugin_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'plugins' . '/';
+		$plugins = array_filter(glob($plugin_dir . '*'), 'is_dir');
+		foreach ($plugins as $plugin) {
+			if ($type == "uninstall") {
+				$this->uninstallPlugin($plugin, $plugin_dir);
+			}
+		}
 
-   /**
-    * 
-    * Function to run after installing the component	 
-    */
-   public function postflight($type, $parent)
-   {
-      $plugin_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'plugins' . '/';
-      $plugins = array_filter(glob($plugin_dir . '*'), 'is_dir');
-      foreach ($plugins as $plugin) {
-         if ($type == "install" || $type == "update") {
-            $this->installPlugin($plugin, $plugin_dir);
-         }
-      }
+		$module_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'modules' . '/';
+		$modules = array_filter(glob($module_dir . '*'), 'is_dir');
+		foreach ($modules as $module) {
+			if ($type == "uninstall") {
+				$this->uninstallModule($module, $module_dir);
+			}
+		}
+	}
 
-      if ($type == "update") {
-         Overrides::fix();
-      }
-   }
+	/**
+	 *
+	 * Function to run after installing the component
+	 */
+	public function postflight($type, $parent)
+	{
+		$plugin_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'plugins' . '/';
+		$plugins = array_filter(glob($plugin_dir . '*'), 'is_dir');
+		foreach ($plugins as $plugin) {
+			if ($type == "install" || $type == "update") {
+				$this->installPlugin($plugin, $plugin_dir);
+			}
+		}
 
-   public function installPlugin($plugin, $plugin_dir)
-   {
-      $db = JFactory::getDbo();
-      $plugin_name = str_replace($plugin_dir, '', $plugin);
+		$module_dir = JPATH_LIBRARIES . '/' . 'astroid' . '/' . 'modules' . '/';
+		$modules = array_filter(glob($module_dir . '*'), 'is_dir');
+		foreach ($modules as $module) {
+			if ($type == "install" || $type == "update") {
+				$this->installModule($module, $module_dir);
+			}
+		}
 
-      $installer = new JInstaller;
-      $installer->install($plugin);
+		if ($type == "update") {
+			Overrides::fix();
+		}
+	}
 
-      $query = $db->getQuery(true);
-      $query->update('#__extensions');
-      $query->set($db->quoteName('enabled') . ' = 1');
-      $query->where($db->quoteName('element') . ' = ' . $db->quote($plugin_name));
-      $query->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
-      $db->setQuery($query);
-      $db->execute();
-      return true;
-   }
+	public function installPlugin($plugin, $plugin_dir)
+	{
+		$db = JFactory::getDbo();
+		$plugin_name = str_replace($plugin_dir, '', $plugin);
 
-   public function uninstallPlugin($plugin, $plugin_dir)
-   {
-      $db = JFactory::getDbo();
-      $plugin_name = str_replace($plugin_dir, '', $plugin);
-      $query = $db->getQuery(true);
-      $query->update('#__extensions');
-      $query->set($db->quoteName('enabled') . ' = 0');
-      $query->where($db->quoteName('element') . ' = ' . $db->quote($plugin_name));
-      $query->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
-      $db->setQuery($query);
-      $db->execute();
-      return true;
-   }
+		$installer = new JInstaller;
+		$installer->install($plugin);
+
+		$query = $db->getQuery(true);
+		$query->update('#__extensions');
+		$query->set($db->quoteName('enabled') . ' = 1');
+		$query->where($db->quoteName('element') . ' = ' . $db->quote($plugin_name));
+		$query->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+		$db->setQuery($query);
+		$db->execute();
+		return true;
+	}
+
+	public function installModule($module, $module_dir)
+	{
+		$db = JFactory::getDbo();
+		$module_name = str_replace($module_dir, '', $module);
+
+		$installer = new JInstaller;
+		$installer->install($module);
+
+		$query = $db->getQuery(true);
+		$query->update('#__extensions');
+		$query->set($db->quoteName('enabled') . ' = 1');
+		$query->where($db->quoteName('element') . ' = ' . $db->quote($module_name));
+		$query->where($db->quoteName('type') . ' = ' . $db->quote('module'));
+		$db->setQuery($query);
+		$db->execute();
+
+		$query = $db->getQuery(true);
+		$query->update('#__modules');
+		$query->set($db->quoteName('published') . ' = 1');
+		$query->where($db->quoteName('module') . ' = ' . $db->quote($module_name));
+		$db->setQuery($query);
+		$db->execute();
+		return true;
+	}
+
+	public function uninstallPlugin($plugin, $plugin_dir)
+	{
+		$db = JFactory::getDbo();
+		$plugin_name = str_replace($plugin_dir, '', $plugin);
+		$query = $db->getQuery(true);
+		$query->update('#__extensions');
+		$query->set($db->quoteName('enabled') . ' = 0');
+		$query->where($db->quoteName('element') . ' = ' . $db->quote($plugin_name));
+		$query->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+		$db->setQuery($query);
+		$db->execute();
+		return true;
+	}
+
+	public function uninstallModule($module, $module_dir)
+	{
+		$db = JFactory::getDbo();
+		$module_name = str_replace($module_dir, '', $module);
+		$query = $db->getQuery(true);
+		$query->update('#__extensions');
+		$query->set($db->quoteName('enabled') . ' = 0');
+		$query->where($db->quoteName('element') . ' = ' . $db->quote($module_name));
+		$query->where($db->quoteName('type') . ' = ' . $db->quote('module'));
+		$db->setQuery($query);
+		$db->execute();
+		return true;
+	}
 }
