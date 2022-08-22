72fa0cdef58339c983085cd238de867f3d1e3e57
Sonny Le <sonlv@templaza.com>
2022-08-22 16:04:27 +0700

    [2.5.18] Fix issue Hide title (and all options)- is changed unintentionally when saving in the frontend

--- plugins/astroid/astroid.php
+++ plugins/astroid/astroid.php
@@ -2,7 +2,7 @@
 /**
  * @package   Astroid Framework
  * @author    TemPlaza https://www.templaza.com
- * @copyright Copyright (C) 2011 - 2021 TemPlaza.
+ * @copyright Copyright (C) 2011 - 2022 TemPlaza.
  * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3 or Later
  */
 /**
@@ -52,6 +52,15 @@ class plgSystemAstroid extends JPlugin
             return false;
         }
         Framework::getClient()->onContentPrepareForm($form, $data);
+    }
+
+    public function onContentBeforeSave($context, $table, $isNew, $data)
+    {
+        if (!file_exists(JPATH_LIBRARIES . '/astroid/framework/library/astroid')) {
+            return false;
+        }
+
+        return Framework::getClient()->onContentBeforeSave($context, $table, $isNew, $data);
     }
 
     public function onAfterRender()
