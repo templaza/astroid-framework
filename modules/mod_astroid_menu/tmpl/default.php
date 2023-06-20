1ef471a8a0de23e9a4be96cc30aeff607383d0a5
Sonny Le <sonlv@templaza.com>
2023-06-20 15:41:48 +0700

    Update data-megamenu for Module

--- modules/mod_astroid_menu/tmpl/default.php
+++ modules/mod_astroid_menu/tmpl/default.php
@@ -23,7 +23,7 @@ if ($tagId = $params->get('tag_id', '')) {
     $id = ' id="' . $tagId . '"';
 }
 ?>
-<div<?php echo $id; ?> class="mod-astroid-menu <?php echo $class_sfx; ?>">
+<div<?php echo $id; ?> data-megamenu data-megamenu-class=".has-megamenu" data-megamenu-content-class=".megamenu-container" data-header-offset="true" data-transition-speed="<?php echo $params->get('dropdown_animation_speed', 300); ?>" data-megamenu-animation="<?php echo $params->get('dropdown_animation_type', 'fade'); ?>" data-easing="<?php echo $params->get('dropdown_animation_ease', 'linear'); ?>" data-astroid-trigger="<?php echo $params->get('dropdown_trigger', 'hover'); ?>" data-megamenu-submenu-class=".nav-submenu" class="mod-astroid-menu <?php echo $class_sfx; ?>">
     <?php
     // header nav starts
     Astroid\Component\Menu::getMenu($menu, $navClass, null, 'left', 'module', $navWrapperClass, $base, $startLevel, $endLevel, $showAllChildren);
