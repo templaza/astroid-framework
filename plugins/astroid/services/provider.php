867889d5b22e24799f0cd315de294116ec86eaec
Sonny Le <sonlv@templaza.com>
2023-11-07 09:27:34 +0700

    Update copyright

--- plugins/astroid/services/provider.php
+++ plugins/astroid/services/provider.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * @package     Joomla.Plugin
- * @subpackage  System.log
- *
- * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   Astroid Framework
+ * @author    Astroid Framework Team https://astroidframe.work
+ * @copyright Copyright (C) 2023 AstroidFrame.work.
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 
 \defined('_JEXEC') or die;
