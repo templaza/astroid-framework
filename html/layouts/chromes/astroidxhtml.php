fed6f9c7f40ed7940689d5e989a93b9a0d1eaeea
Sonny Le <sonlv@templaza.com>
2023-11-20 09:34:50 +0700

    Update astroidxhtml.php

--- html/layouts/chromes/astroidxhtml.php
+++ html/layouts/chromes/astroidxhtml.php
@@ -8,7 +8,44 @@
  */
 
 defined('_JEXEC') or die;
-use Joomla\CMS\Layout\FileLayout;
 
-$renderer = new FileLayout('chromes.astroidxhtml', JPATH_LIBRARIES . '/astroid/framework/layouts');
-echo $renderer->render($displayData);
+use Joomla\Utilities\ArrayHelper;
+
+$module  = $displayData['module'];
+$params  = $displayData['params'];
+$attribs = $displayData['attribs'];
+
+if ($module->content === null || $module->content === '') {
+    return;
+}
+
+$moduleTag              = $params->get('module_tag', 'div');
+$moduleAttribs          = [];
+$moduleAttribs['class'] = $module->position . ' moduletable ' . htmlspecialchars($params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$headerTag              = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
+$headerClass            = htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
+$headerAttribs          = [];
+
+// Only output a header class if one is set
+if ($headerClass !== '') {
+    $headerAttribs['class'] = $headerClass;
+}
+
+// Only add aria if the moduleTag is not a div
+if ($moduleTag !== 'div') {
+    if ($module->showtitle) :
+        $moduleAttribs['aria-labelledby'] = 'mod-' . $module->id;
+        $headerAttribs['id']              = 'mod-' . $module->id;
+    else :
+        $moduleAttribs['aria-label'] = $module->title;
+    endif;
+}
+
+$header = '<' . $headerTag . ' ' . ArrayHelper::toString($headerAttribs) . '>' . $module->title . '</' . $headerTag . '>';
+?>
+<<?php echo $moduleTag; ?> <?php echo ArrayHelper::toString($moduleAttribs); ?>>
+<?php if ($module->showtitle) : ?>
+    <?php echo $header; ?>
+<?php endif; ?>
+<?php echo $module->content; ?>
+</<?php echo $moduleTag; ?>>
