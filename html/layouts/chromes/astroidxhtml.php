76518c12df0691da4e4ab41e4ff3575ec34d564a
Sonny Le <sonlv@templaza.com>
2023-12-25 09:34:12 +0700

    Fix issue #486

--- html/layouts/chromes/astroidxhtml.php
+++ html/layouts/chromes/astroidxhtml.php
@@ -7,11 +7,12 @@
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 defined('_JEXEC') or die;
-
+use Joomla\CMS\Layout\FileLayout;
 /**
  * TO OVERRIDE THIS LAYOUT
  * 1. Remove line "require Astroid\Helper\Overrides::getHTMLTemplate()";
  * 2. Copy source code from libraries/astroid/framework/html/{find a file that you will override here}
  * 3. Paste source code to below and start to edit.
  */
-require Astroid\Helper\Overrides::getHTMLTemplate();
+$renderer = new FileLayout('chromes.astroidxhtml', JPATH_LIBRARIES . '/astroid/framework/html/layouts');
+echo $renderer->render($displayData);
