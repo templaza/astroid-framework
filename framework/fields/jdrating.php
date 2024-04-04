04de2426691d806cbe986fd3115ba403a1ffa65b
Sonny Le <sonlv@templaza.com>
2024-04-04 12:26:00 +0700

    Add GPL license for Subform & JD Rating

--- framework/fields/jdrating.php
+++ framework/fields/jdrating.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @package   Astroid Framework
+ * @author    Astroid Framework Team https://astroidframe.work
+ * @copyright Copyright (C) 2023 AstroidFrame.work.
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ */
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
