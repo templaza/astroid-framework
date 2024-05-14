53c76f55b7018b7a0ba1c6b79c86a1ea52c228b8
Sonny Le <sonlv@templaza.com>
2024-05-14 17:16:00 +0700

    Add Testimonial Widget

---
+++ framework/fields/astroidborder.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @package   Astroid Framework
+ * @author    Astroid Framework Team https://astroidframe.work
+ * @copyright Copyright (C) 2024 AstroidFrame.work.
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ */
+defined('JPATH_PLATFORM') or die;
+use Joomla\CMS\Form\FormField;
+/**
+ * Color Form Field class for the Joomla Platform.
+ * This implementation is designed to be compatible with HTML5's `<input type="color">`
+ *
+ * @link   https://www.w3.org/TR/html-markup/input.color.html
+ * @since  11.3
+ */
+class JFormFieldAstroidBorder extends FormField
+{
+
+    /**
+     * The form field type.
+     *
+     * @var    string
+     * @since  11.3
+     */
+    protected $type = 'AstroidBorder';
+
+    /**
+     * Method to get the field input markup.
+     *
+     * @return  string  The field input markup.
+     *
+     * @since   11.3
+     */
+    protected function getInput()
+    {
+        $json =   [
+            'id'      =>  $this->id,
+            'name'    =>  $this->name,
+            'value'   =>  $this->value,
+            'type'    =>  strtolower($this->type),
+        ];
+        return json_encode($json);
+    }
+}
