7ca901c38cb1082e10894cf7863baf7197616590
Sonny Le <sonlv@templaza.com>
2024-07-29 12:18:09 +0700

    Remove unnecessary code

--- framework/html/com_content/category/blog_item.php
+++ framework/html/com_content/category/blog_item.php
@@ -73,7 +73,7 @@ $isUnpublished = ($this->item->state == ContentComponent::CONDITION_UNPUBLISHED 
 $clsItemContainer   = $astroidArticle->getStyle('container');
 $clsItemBody        = $astroidArticle->getStyle('body');
 ?>
-<div class="item-content position-relative d-flex flex-column<?php echo (!empty($clsItemContainer) ? ' '.$clsItemContainer : ''); echo ($image_position == 'left' || $image_position == 'right') ? ' border-top media-'.$image_position : ''; ?>">
+<div class="item-content position-relative<?php echo (!empty($clsItemContainer) ? ' '.$clsItemContainer : ''); echo ($image_position == 'left' || $image_position == 'right') ? ' border-top media-'.$image_position : ''; ?>">
     <?php if ($isUnpublished) : ?>
     <div class="system-unpublished">
     <?php endif; ?>
