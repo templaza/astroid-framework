e6f953d157a871290d7c5ef6b153c69994ba1f05
Sonny Le <sonlv@templaza.com>
2024-08-19 11:13:45 +0700

    Improve animation of Color Transform

--- js/colortransform.js
+++ js/colortransform.js
@@ -1,5 +1,8 @@
 (function ($) {
-    window.addEventListener("load", (event) => {
+    $(document).ready(function (){
+        $('body').addClass('as-transition-body');
+    })
+    $(window).on('load', function () {
         window.onscroll = function(ev) {
             if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight * (ASTROID_COLOR_TRANSFORM.offset/100)) {
                 if ($('html').attr('data-bs-theme') === ASTROID_COLOR_TRANSFORM.from) {
@@ -11,5 +14,5 @@
                 }
             }
         };
-    });
+    })
 }(jQuery));
