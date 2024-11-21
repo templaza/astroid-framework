da56c3250b6aed868cd6993b045b5f0e9834fed4
Sonny Le <sonlv@templaza.com>
2024-11-21 14:35:20 +0700

    Add update branch

--- assets/vendor/freetemplates/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-const divroot = document.createElement("div");
-divroot.setAttribute('id', 'as-free-templates');
-document.body.appendChild(divroot);
-createApp(App).mount('#as-free-templates');
--- assets/vendor/selectsublayout/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-let as_sublayouts = document.querySelectorAll('.as-select-sublayout');
-as_sublayouts.forEach(sublayout => {
-    createApp(App, {
-        sublayout_id : sublayout.id
-    }).mount('#'+sublayout.id);
-});
