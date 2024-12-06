d2d1203e696019d0627782377c91f5f9124e1213
Sonny Le <sonlv@templaza.com>
2024-12-06 16:56:17 +0700

    Update script for Module layout builder

---
+++ assets/vendor/manager/src/Module.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onBeforeMount, ref, provide } from 'vue';
+import axios from "axios";
+import { faCircle, faArrowsLeftRight, faTrash, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import Layout from "./components/helpers/Layout.vue";
+import './assets/base.scss'
+const data = JSON.parse(document.getElementById('astroid_layout_module_json').innerHTML);
+const layout = ref('{"sections":[]}');
+provide('constant', data.constant);
+onBeforeMount(()=>{
+    layout.value = JSON.stringify(data.layout);
+})
+</script>
+<template>
+    <Layout v-model="layout" source="joomla_module" :field="{
+                id: data.id,
+                input: {
+                    id: data.id,
+                    name: data.name,
+                    value: data.layout
+                }
+            }" />
+</template>
