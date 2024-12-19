a8e2536ada2863e778fbc6183585b3a3de0cdf73
Sonny Le <sonlv@templaza.com>
2024-12-19 10:18:04 +0700

    Update astroidlayoutmodule.php

--- framework/fields/astroidlayoutmodule.php
+++ framework/fields/astroidlayoutmodule.php
@@ -31,30 +31,24 @@ class JFormFieldAstroidLayoutModule extends FormField {
 
     protected function getInput() {
         $app    =   Factory::getApplication();
-        $option =   $app->input->get('option', '');
-        if (($option == 'com_modules' || $option == 'com_advancedmodules')
-            && $app->input->get('view', '') == 'module'
-            && $app->input->get('layout', '') == 'edit') {
-            $id     =   $app->input->get('id');
-            $layout =   Layout::loadModuleLayout($id);
-            $constant   =   Helper\Constants::manager_configs('joomla_module');
-            $json = [
-                'id'      =>  $this->id,
-                'name' => $this->name,
-                'layout' => $layout,
-                'constant'   => $constant,
-            ];
-            $html   =   '<script type="application/json" id="astroid_layout_module_json">'.json_encode($json).'</script>';
-            $html   .=  '<div id="astroid-layout-module" class="astroid-layout border rounded p-4"></div>';
-            $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
-            $wa->registerAndUseStyle('astroid.manager', "media/astroid/assets/vendor/manager/dist/index.css");
-            $wa->registerAndUseStyle('astroid.icons', "media/astroid/assets/vendor/linearicons/font.min.css");
-            $wa->useScript('bootstrap.tab');
-            $wa->useScript('bootstrap.modal');
-            $wa->registerAndUseScript('astroid.manager', 'media/astroid/assets/vendor/manager/dist/index.js', ['relative' => true, 'version' => 'auto'], ['type' => 'module']);
-            $wa->addInlineScript("(function ($) { $(document).ready(function () { $('#astroid-layout-module').parent().prev().remove(); }); })(jQuery)");
-            return $html;
-        }
-        return 'This field is only available in module edit layout.';
+        $id     =   $app->input->get('id');
+        $layout =   Layout::loadModuleLayout($id);
+        $constant   =   Helper\Constants::manager_configs('joomla_module');
+        $json = [
+            'id'      =>  $this->id,
+            'name' => $this->name,
+            'layout' => $layout,
+            'constant'   => $constant,
+        ];
+        $html   =   '<script type="application/json" id="astroid_layout_module_json">'.json_encode($json).'</script>';
+        $html   .=  '<div id="astroid-layout-module" class="astroid-layout border rounded p-4"></div>';
+        $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+        $wa->registerAndUseStyle('astroid.manager', "media/astroid/assets/vendor/manager/dist/index.css");
+        $wa->registerAndUseStyle('astroid.icons', "media/astroid/assets/vendor/linearicons/font.min.css");
+        $wa->useScript('bootstrap.tab');
+        $wa->useScript('bootstrap.modal');
+        $wa->registerAndUseScript('astroid.manager', 'media/astroid/assets/vendor/manager/dist/index.js', ['relative' => true, 'version' => 'auto'], ['type' => 'module']);
+        $wa->addInlineScript("(function ($) { $(document).ready(function () { $('#astroid-layout-module').parent().prev().remove(); }); })(jQuery)");
+        return $html;
     }
 }
