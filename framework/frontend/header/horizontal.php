d58f713f9c58d5aeddc0559708de7173795a7c5b
Sonny Le <sonlv@templaza.com>
2025-07-16 14:18:40 +0700

    Update horizontal.php

--- framework/frontend/header/horizontal.php
+++ framework/frontend/header/horizontal.php
@@ -104,7 +104,7 @@ $headAttrs = $header_menu_method == 'default' ? ' data-megamenu data-megamenu-cl
       }
       ?>
       <?php if ($block_1_type != 'blank' || $mode == 'right' || $enable_offcanvas) : ?>
-         <div class="header-right-section as-gutter-x-xl@lg d-none d-<?php echo $header_breakpoint; ?>-flex justify-content-end<?php echo $mode == 'right' ? ' flex-'.$header_breakpoint.'-grow-1' : ''; ?>">
+         <div class="header-right-section as-gutter-x-xl@lg <?php echo ($enable_offcanvas ? 'd-flex' : 'd-'.$header_breakpoint.'-flex d-none'); ?> justify-content-end<?php echo $mode == 'right' ? ' flex-'.$header_breakpoint.'-grow-1' : ''; ?>">
             <?php
             if ($mode == 'right') {
                // header nav starts
