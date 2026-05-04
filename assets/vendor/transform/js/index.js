2dcc36415e7331efcc2f9686a17be601138f3b59
Sonny Le <sonlv@templaza.com>
2026-05-04 11:12:58 +0700

    Add Filter option to Transform

--- assets/vendor/transform/js/index.js
+++ assets/vendor/transform/js/index.js
@@ -34,6 +34,40 @@ class AstroidTransform {
         const tl = gsap.timeline(this.timelineConfig);
         // Build scenes
         this.scenes.forEach(scene => {
+            // Support filter-related properties
+            ['from', 'to'].forEach(type => {
+                if (!scene[type]) return;
+
+                const filters = [];
+
+                if (typeof scene[type].blur !== 'undefined') {
+                    const v = scene[type].blur;
+                    filters.push(`blur(${typeof v === 'number' ? v + 'px' : v})`);
+                    delete scene[type].blur;
+                }
+
+                if (typeof scene[type].brightness !== 'undefined') {
+                    const v = scene[type].brightness;
+                    filters.push(`brightness(${v})`);
+                    delete scene[type].brightness;
+                }
+
+                if (typeof scene[type].contrast !== 'undefined') {
+                    const v = scene[type].contrast;
+                    filters.push(`contrast(${v})`);
+                    delete scene[type].contrast;
+                }
+
+                if (typeof scene[type].grayscale !== 'undefined') {
+                    const v = scene[type].grayscale;
+                    filters.push(`grayscale(${v})`);
+                    delete scene[type].grayscale;
+                }
+
+                if (filters.length) {
+                    scene[type].filter = filters.join(' ');
+                }
+            });
             if (scene.from && scene.to) {
                 tl.fromTo(this.el, scene.from, scene.to);
             } else if (scene.from) {
