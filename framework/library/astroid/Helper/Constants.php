c4b341ad2022c3cb125d17f24a077ec15e3d5397
Sonny Le <sonlv@templaza.com>
2026-07-22 15:35:07 +0700

    Update FontAwesome 7.3.1

--- framework/library/astroid/Helper/Constants.php
+++ framework/library/astroid/Helper/Constants.php
@@ -19,8 +19,8 @@ defined('_JEXEC') or die;
 
 class Constants 
 {
-    public static $astroid_version = '3.4.2';
-    public static $fontawesome_version = '7.2.0';
+    public static $astroid_version = '3.4.3-rc1';
+    public static $fontawesome_version = '7.3.1';
     public static $fancybox_version = '6.1';
     public static $animatecss_version = '3.7.0';
     public static $forum_link = 'https://github.com/templaza/astroid-framework/issues';
