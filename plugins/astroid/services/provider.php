353bdb65894ff6d19f5058164481be8839440f49
Sonny Le <sonlv@templaza.com>
2026-07-29 11:54:54 +0700

    Fix error call Library when Astroid did not finish installation process

--- plugins/astroid/services/provider.php
+++ plugins/astroid/services/provider.php
@@ -7,13 +7,7 @@
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 
-\defined('_JEXEC') or die;
-require_once JPATH_ROOT . DIRECTORY_SEPARATOR .'libraries'
-    .DIRECTORY_SEPARATOR.'astroid'
-    .DIRECTORY_SEPARATOR.'framework'
-    .DIRECTORY_SEPARATOR.'library'
-    .DIRECTORY_SEPARATOR.'vendor'
-    .DIRECTORY_SEPARATOR.'autoload.php';
+\defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
@@ -21,6 +15,26 @@ use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 use Joomla\Plugin\System\Astroid\Extension\AstroidPlugin;
+
+$autoloadPath = JPATH_ROOT . DIRECTORY_SEPARATOR .'libraries'
+    .DIRECTORY_SEPARATOR.'astroid'
+    .DIRECTORY_SEPARATOR.'framework'
+    .DIRECTORY_SEPARATOR.'library'
+    .DIRECTORY_SEPARATOR.'vendor'
+    .DIRECTORY_SEPARATOR.'autoload.php';
+
+// Only require the autoload file if it exists, otherwise log a warning and return an empty provider
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+} else {
+    // Log a warning message to the error log
+    error_log('Warning: Astroid Framework libraries are not properly installed. Path: ' . $autoloadPath);
+
+    // Return an empty provider to avoid crashing
+    return new class () implements ServiceProviderInterface {
+        public function register(Container $container): void {}
+    };
+}
 
 return new class () implements ServiceProviderInterface {
     /**
