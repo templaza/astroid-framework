085b0928bdd84a23c86eedba2873ceb6865751dc
Sonny Le <sonlv@templaza.com>
2026-09-16 08:33:59 +0700

    Update composer library

--- framework/library/vendor/composer/autoload_static.php
+++ framework/library/vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInita7f239344f0f7f935f9220eec1fb9c70
 {
     public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
     );
